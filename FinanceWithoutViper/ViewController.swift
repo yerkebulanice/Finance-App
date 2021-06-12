12962a9daa52f8307ae5b1a1f0b4259aafcf9367
Kapitan <erkebulanice@gmail.com>
2021-06-12 20:05:39 +0500

    Initial Commit

---
+++ FinanceWithoutViper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinanceWithoutViper
+//
+//  Created by Еркебулан on 12.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
